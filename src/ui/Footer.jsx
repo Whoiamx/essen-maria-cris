e2ec749a004c01a8ed4c084feb122ee067a335e6
Whoiamx <gaastontimchuk@gmail.com>
2024-11-24 14:45:04 -0300

    ajustes de cosmetica dentro del footer

--- src/ui/Footer.jsx
+++ src/ui/Footer.jsx
@@ -1,19 +1,74 @@
+import Accordion from "@mui/material/Accordion";
+
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+
 export const Footer = () => {
   return (
     <div className="footer">
       <div className="comprando">
-        <p className="title-footer">Comprando</p>
-        <ul className="footer-list">
-          <li>
-            <a href="#">Cómo son las formas de pago?</a>
-          </li>
-          <li>
-            <a href="#">Cómo son los medios de envío?</a>
-          </li>
-          <li>
-            <a href="#">Cómo funcionan los cambios?</a>
-          </li>
-        </ul>
+        <h4 className="title-comprando">Preguntas Frecuentes</h4>
+        <Accordion sx={{ width: "85%" }}>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon sx={{ color: "white" }} />}
+            aria-controls="panel1-content"
+            id="panel1-header"
+            sx={{ backgroundColor: "#771b1b", color: "white" }}
+          >
+            ¿Qué formas de pago aceptan?
+          </AccordionSummary>
+          <AccordionDetails>
+            Aceptamos tarjetas de crédito y débito, transferencias bancarias, y
+            algunos medios digitales como Mercado Pago. Ofrecemos promociones
+            con tarjetas seleccionadas, ¡consultá nuestras ofertas vigentes!
+          </AccordionDetails>
+        </Accordion>
+        <Accordion sx={{ width: "85%" }}>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon sx={{ color: "white" }} />}
+            aria-controls="panel2-content"
+            id="panel2-header"
+            sx={{ backgroundColor: "#771b1b", color: "white" }}
+          >
+            ¿Tienen cuotas sin interés?
+          </AccordionSummary>
+          <AccordionDetails>
+            Sí, contamos con opciones de financiación en cuotas sin interés con
+            tarjetas participantes. La disponibilidad y cantidad de cuotas
+            dependen de las promociones vigentes.
+          </AccordionDetails>
+        </Accordion>
+        <Accordion sx={{ width: "85%" }}>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon sx={{ color: "white" }} />}
+            aria-controls="panel3-content"
+            id="panel3-header"
+            sx={{ backgroundColor: "#771b1b", color: "white" }}
+          >
+            ¿Puedo comprar para revender?
+          </AccordionSummary>
+          <AccordionDetails defaultExpanded>
+            ¡Claro! Si estás interesado en revender nuestras ollas, escribinos
+            para recibir más información sobre precios mayoristas y beneficios
+            exclusivos.
+          </AccordionDetails>
+        </Accordion>
+        <Accordion sx={{ width: "85%" }}>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon sx={{ color: "white" }} />}
+            aria-controls="panel3-content"
+            id="panel3-header"
+            sx={{ backgroundColor: "#771b1b", color: "white" }}
+          >
+            ¿Qué garantía tienen las ollas Essen?
+          </AccordionSummary>
+          <AccordionDetails defaultExpanded>
+            Todas nuestras ollas Essen cuentan con garantía oficial de 2 años.
+            Si surge algún problema, podés contactarnos para coordinar la
+            solución.
+          </AccordionDetails>
+        </Accordion>
       </div>
       <div className="contact-us">
         <p className="title-footer">Tenes alguna pregunta?</p>
